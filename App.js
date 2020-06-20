61dd860b49020bd25e4e9f290b62590330cb81d2
brians123 <brianssui@yahoo.com>
2020-06-20 16:21:13 -0400

    styled log in page and fixed this.state issue with onChangeText

--- App.js
+++ App.js
@@ -1,30 +1,56 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Tinder from './components/Tinder';
-import Search from './components/Search';
-import HomeScreen from './components/HomeScreen';
-import CreateRoom from './components/CreateRoom';
-import JoinRoom from './components/JoinRoom.js';
-import MyRooms from './components/MyRooms';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Tinder from "./screens/Tinder";
+import Search from "./screens/Search";
+import HomeScreen from "./screens/HomeScreen";
+import CreateRoom from "./screens/CreateRoom";
+import JoinRoom from "./screens/JoinRoom.js";
+import MyRooms from "./screens/MyRooms";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
 
-import StartSignUp from './components/SignUp/StartSignUp';
-import NameSignUp from './components/SignUp/NameSignUp';
-import EmailSignUp from './components/SignUp/EmailSignUp';
-import LocationSignUp from './components/SignUp/LocationSignUp';
-import DoneSignUp from './components/SignUp/DoneSignUp';
-import BirthdaySignUp from './components/SignUp/BirthdaySignUp';
+import StartSignUp from "./screens/SignUp/StartSignUp";
+import NameSignUp from "./screens/SignUp/NameSignUp";
+import EmailSignUp from "./screens/SignUp/EmailSignUp";
+import LocationSignUp from "./screens/SignUp/LocationSignUp";
+import DoneSignUp from "./screens/SignUp/DoneSignUp";
+import BirthdaySignUp from "./screens/SignUp/BirthdaySignUp";
+import "./global.js";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "karla-bold": require("./assets/fonts/Karla-Bold.ttf"),
+    "karla-italic": require("./assets/fonts/Karla-Italic.ttf"),
+    "karla-regular": require("./assets/fonts/Karla-Regular.ttf"),
+  });
+};
 
 // set up navigation
 const Stack = createStackNavigator();
 
 function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="PhoneNumberLogin" component={Login} />
+        <Stack.Screen
+          name="PhoneNumberLogin"
+          component={Login}
+          options={{ title: "Login", headerShown: false }}
+        />
         <Stack.Screen name="StartSignUp" component={StartSignUp} />
         <Stack.Screen name="NameSignUp" component={NameSignUp} />
         <Stack.Screen name="BirthdaySignUp" component={BirthdaySignUp} />
@@ -34,7 +60,11 @@ function App() {
 
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Main Menu' }} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Main Menu" }}
+        />
         <Stack.Screen name="Search" component={Search} />
         <Stack.Screen name="Tinder" component={Tinder} />
         <Stack.Screen name="JoinRoom" component={JoinRoom} />
---
+++ global.js
@@ -0,0 +1,7 @@
+import { Dimensions } from "react-native";
+
+global.orange = "#FF8364";
+global.gray = "#EDEDED";
+global.yellow = "#FFEBC2";
+global.deviceWidth = Dimensions.get("window").width;
+global.deviceHeight = Dimensions.get("window").height;
