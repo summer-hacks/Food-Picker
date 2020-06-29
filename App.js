e38caff26701b1b094551ff08a95f06014e23b51
janethuangg <jh6248@nyu.edu>
2020-06-28 21:31:11 -0400

    failed attempt at address autocomplete

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Tinder from './components/Tinder';
-import Search from './components/Search';
-import HomeScreen from './components/HomeScreen';
-import CreateRoom from './components/CreateRoom';
-import JoinRoom from './components/JoinRoom.js';
-import MyRooms from './components/MyRooms.js';
-import RoomPage from './components/RoomPage.js';
-import PartyInfo from './components/PartyInfo.js';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Tinder from "./components/Tinder";
+import Search from "./components/Search";
+import HomeScreen from "./components/HomeScreen";
+import CreateRoom from "./components/CreateRoom";
+import JoinRoom from "./components/JoinRoom.js";
+import MyRooms from "./components/MyRooms.js";
+import RoomPage from "./components/RoomPage.js";
+import PartyInfo from "./components/PartyInfo.js";
 
 // set up navigation
 const Stack = createStackNavigator();
@@ -17,7 +17,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Main Menu' }} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Main Menu" }}
+        />
         <Stack.Screen name="PartyInfo" component={PartyInfo} />
         <Stack.Screen name="Search" component={Search} />
         <Stack.Screen name="Tinder" component={Tinder} />
--- components/Search.js
+++ components/Search.js
@@ -1,217 +1,324 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Image, TextInput, TouchableOpacity, Alert,Keyboard} from 'react-native';
-import * as Location from 'expo-location';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Keyboard,
+} from "react-native";
+import * as Location from "expo-location";
 
-// prob shouldn't be hardcoding api key in here but works for now 
-const api_key = 'rfzFsGmwjhmXJqBMeXgjk8VTwpz8zevZE0xPzGz2YAzDiP15VI5alXOxkDD_GlFneIOTsee7mp5RYx5DVb10CJOlNw58NqlfmwItWr4D5NzfFWge7XEnp8kNrE7UXnYx'
-const url = 'https://api.yelp.com/v3/businesses/search?'
+// prob shouldn't be hardcoding api key in here but works for now
+const api_key =
+  "rfzFsGmwjhmXJqBMeXgjk8VTwpz8zevZE0xPzGz2YAzDiP15VI5alXOxkDD_GlFneIOTsee7mp5RYx5DVb10CJOlNw58NqlfmwItWr4D5NzfFWge7XEnp8kNrE7UXnYx";
+const url = "https://api.yelp.com/v3/businesses/search?";
 
 const Search = ({ route, navigation }) => {
-    const { user } = route.params;
-    const { partySize } = route.params;
-    const { partyName } = route.params;
+  const { user } = route.params;
+  const { partySize } = route.params;
+  const { partyName } = route.params;
 
-    const [location, setLocation] = useState('')
-    const [latitude, setLatitude] = useState(0)
-    const [longitude, setLongitude] = useState(0)
-    const [radius, setRadius] = useState(0)
-    const [maxRes, setMaxRes] = useState(0)
-    const [dollars, setDollars] = useState([])
-    const [$clicked, set$] = useState(false)
-    const [$$clicked, set$$] = useState(false)
-    const [$$$clicked, set$$$] = useState(false)
-    const [$$$$clicked, set$$$$] = useState(false)
+  const [location, setLocation] = useState("");
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [radius, setRadius] = useState(0);
+  const [maxRes, setMaxRes] = useState(0);
+  const [dollars, setDollars] = useState([]);
+  const [$clicked, set$] = useState(false);
+  const [$$clicked, set$$] = useState(false);
+  const [$$$clicked, set$$$] = useState(false);
+  const [$$$$clicked, set$$$$] = useState(false);
 
-    const getCurrLocation = async() => {
-      console.log('clicked')
-      let { status } = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Permission to access location was denied');
+  const getCurrLocation = async () => {
+    console.log("clicked");
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Permission to access location was denied");
+    } else {
+      let location = await Location.getCurrentPositionAsync({});
+      setLongitude(location.coords.longitude);
+      setLatitude(location.coords.latitude);
+      setLocation("Current Location");
+    }
+  };
+
+  // helper functions to handle user input
+  const onChangeLocation = (location) => {
+    setLocation(location);
+  };
+
+  const onChangeRadius = (radius) => {
+    setRadius(radius);
+  };
+
+  const onChangeMaxRes = (maxRes) => {
+    setMaxRes(maxRes);
+  };
+
+  const handle$ = () => {
+    Keyboard.dismiss();
+    if ($clicked) {
+      setDollars((prev) => prev.filter((ele) => ele !== 1));
+    } else {
+      setDollars((prev) => [...prev, 1]);
+    }
+    set$((prev) => !prev);
+  };
+
+  const handle$$ = () => {
+    Keyboard.dismiss();
+    if ($$clicked) {
+      setDollars((prev) => prev.filter((ele) => ele !== 2));
+    } else {
+      setDollars((prev) => [...prev, 2]);
+    }
+    set$$((prev) => !prev);
+  };
+
+  const handle$$$ = () => {
+    Keyboard.dismiss();
+    if ($$$clicked) {
+      setDollars((prev) => prev.filter((ele) => ele !== 3));
+    } else {
+      setDollars((prev) => [...prev, 3]);
+    }
+    set$$$((prev) => !prev);
+  };
+
+  const handle$$$$ = () => {
+    Keyboard.dismiss();
+    if ($$$$clicked) {
+      setDollars((prev) => prev.filter((ele) => ele !== 4));
+    } else {
+      setDollars((prev) => [...prev, 4]);
+    }
+    set$$$$((prev) => !prev);
+  };
+
+  // get restaurant data via Yelp API (this is passed to the CreateRoom component and then stored in firebase)
+  // submitting blank form raises error
+  const getData = async (
+    location,
+    longitude,
+    latitude,
+    radius,
+    maxRes,
+    dollars
+  ) => {
+    const prices = dollars.join(",");
+    if (!location || !radius || !maxRes) {
+      Alert.alert(
+        "Empty field",
+        "Please enter all info",
+        [
+          {
+            text: "Ok",
+            style: "cancel",
+          },
+        ],
+        { cancelable: true }
+      );
+    } else {
+      let full_url = "";
+      if (location == "Current Location") {
+        full_url =
+          url +
+          `latitude=${latitude}&longitude=${longitude}&radius=${radius}&categories=restaurants&limit=${maxRes}&price=${prices}`;
       } else {
-        let location = await Location.getCurrentPositionAsync({});
-        setLongitude(location.coords.longitude)
-        setLatitude(location.coords.latitude)
-        setLocation("Current Location");
+        full_url =
+          url +
+          `location=${location}&radius=${radius}&categories=restaurants&limit=${maxRes}&price=${prices}`;
       }
+
+      const res = await fetch(full_url, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + api_key,
+        },
+      });
+      const resJson = await res.json();
+      return resJson.businesses;
     }
+  };
+  let defaultLocation = "";
 
-    // helper functions to handle user input
-    const onChangeLocation = location => {
-      setLocation(location);
-    }
+  if (longitude && latitude) {
+    defaultLocation = "Current Location";
+  }
 
-    const onChangeRadius = radius => {
-      setRadius(radius);
-    }
-
-    const onChangeMaxRes = maxRes => {
-      setMaxRes(maxRes);
-    }
-
-    const handle$ = () => {
-      Keyboard.dismiss()
-      if ($clicked){
-        setDollars(prev => prev.filter(ele => ele !== 1))
-      } else {
-        setDollars(prev => [...prev, 1])
-      }
-      set$(prev => !prev)
-    }
-
-    const handle$$ = () => {
-      Keyboard.dismiss()
-      if ($$clicked){
-        setDollars(prev => prev.filter(ele => ele !== 2))
-      } else {
-        setDollars(prev => [...prev, 2])
-      }
-      set$$(prev => !prev)
-    }
-
-    const handle$$$ = () => {
-      Keyboard.dismiss()
-      if ($$$clicked){
-        setDollars(prev => prev.filter(ele => ele !== 3))
-      } else {
-        setDollars(prev => [...prev, 3])
-      }
-      set$$$(prev => !prev)
-    }
-
-    const handle$$$$ = () => {
-      Keyboard.dismiss()
-      if ($$$$clicked){
-        setDollars(prev => prev.filter(ele => ele !== 4))
-      } else {
-        setDollars(prev => [...prev, 4])
-      }
-      set$$$$(prev => !prev)
-    }
-
-    // get restaurant data via Yelp API (this is passed to the CreateRoom component and then stored in firebase)
-    // submitting blank form raises error
-    const getData = async(location, longitude, latitude, radius, maxRes, dollars) => {   
-      const prices = dollars.join(',')
-      if (!location || !radius || !maxRes) {
-        Alert.alert(
-          'Empty field',
-          'Please enter all info',
-          [
-            {
-              text: 'Ok',
-              style: 'cancel',
-            },
-          ],
-          {cancelable: true},
-        );
-      } else {
-        let full_url = ""
-        if (location == "Current Location"){
-          full_url = url + `latitude=${latitude}&longitude=${longitude}&radius=${radius}&categories=restaurants&limit=${maxRes}&price=${prices}`
-        } else {
-          full_url = url + `location=${location}&radius=${radius}&categories=restaurants&limit=${maxRes}&price=${prices}`
-        }
-
-        const res = await fetch(full_url, {
-          method: "GET",
-          headers: {
-            Authorization: "Bearer " + api_key
-          }
-        })
-        const resJson = await res.json();
-        return resJson.businesses
-        }
-    }
-    let defaultLocation = ""
-
-    if (longitude && latitude) {
-      defaultLocation = "Current Location"
-    }
-
-    return(
+  return (
     <View style={styles.container}>
-      <View style={{flexDirection:'row',alignItems:'center', justifyContent:'center'}}>
-        <View style={{flex:4}}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View style={{ flex: 4 }}>
           <TextInput
-          placeholder="Location" 
-          style={styles.input}
-          onChangeText={onChangeLocation}
-          defaultValue={defaultLocation}
-          value={location}
+            placeholder="Location"
+            style={styles.input}
+            onChangeText={onChangeLocation}
+            defaultValue={defaultLocation}
+            value={location}
           />
         </View>
-        <View style={{flex:1}}>
-          <Button title="CL"onPress={getCurrLocation}/>
+        <View style={{ flex: 1 }}>
+          <Button title="CL" onPress={getCurrLocation} />
         </View>
       </View>
-      <TextInput 
-        placeholder="Radius (meters)" 
-        keyboardType={'numeric'}
+      <TextInput
+        placeholder="Radius (meters)"
+        keyboardType={"numeric"}
         style={styles.input}
         onChangeText={onChangeRadius}
       />
-      <TextInput 
-        placeholder="Max Number of Restaurants" 
-        keyboardType={'numeric'}
+      <TextInput
+        placeholder="Max Number of Restaurants"
+        keyboardType={"numeric"}
         style={styles.input}
         onChangeText={onChangeMaxRes}
       />
-      <View style={{flex: 1, flexDirection: 'row', justifyContent:'space-evenly'}}>
-        <TouchableOpacity style={{height: 50,width: 50, justifyContent: 'center',borderRadius: 10, backgroundColor: $clicked ? 'salmon' : 'white' }} onPress={handle$}>
-          <Text style={{textAlign: 'center', color: $clicked ? 'white' : 'salmon'}}>$</Text>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: 50,
+            justifyContent: "center",
+            borderRadius: 10,
+            backgroundColor: $clicked ? "salmon" : "white",
+          }}
+          onPress={handle$}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: $clicked ? "white" : "salmon",
+            }}
+          >
+            $
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{height: 50,width: 50, justifyContent: 'center',borderRadius: 10, backgroundColor: $$clicked ? 'salmon' : 'white' }} onPress={handle$$}>
-          <Text style={{textAlign: 'center', color: $$clicked ? 'white' : 'salmon'}}>$$</Text>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: 50,
+            justifyContent: "center",
+            borderRadius: 10,
+            backgroundColor: $$clicked ? "salmon" : "white",
+          }}
+          onPress={handle$$}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: $$clicked ? "white" : "salmon",
+            }}
+          >
+            $$
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{height: 50,width: 50, justifyContent: 'center',borderRadius: 10, backgroundColor: $$$clicked ? 'salmon' : 'white' }} onPress={handle$$$}>
-          <Text style={{textAlign: 'center', color: $$$clicked ? 'white' : 'salmon'}}>$$$</Text>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: 50,
+            justifyContent: "center",
+            borderRadius: 10,
+            backgroundColor: $$$clicked ? "salmon" : "white",
+          }}
+          onPress={handle$$$}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: $$$clicked ? "white" : "salmon",
+            }}
+          >
+            $$$
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{height: 50,width: 50, justifyContent: 'center',borderRadius: 10, backgroundColor: $$$$clicked ? 'salmon' : 'white' }} onPress={handle$$$$}>
-          <Text style={{textAlign: 'center', color: $$$$clicked ? 'white' : 'salmon'}}>$$$$</Text>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: 50,
+            justifyContent: "center",
+            borderRadius: 10,
+            backgroundColor: $$$$clicked ? "salmon" : "white",
+          }}
+          onPress={handle$$$$}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: $$$$clicked ? "white" : "salmon",
+            }}
+          >
+            $$$$
+          </Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.btn} onPress={async() => {
-         const data = await getData(location, longitude, latitude, radius, maxRes, dollars);
-         navigation.navigate('CreateRoom', {
-          restaurants: data,
-          partySize: partySize,
-          partyName: partyName,
-          user: user
-        });
-        }
-      }>
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={async () => {
+          const data = await getData(
+            location,
+            longitude,
+            latitude,
+            radius,
+            maxRes,
+            dollars
+          );
+          navigation.navigate("CreateRoom", {
+            restaurants: data,
+            partySize: partySize,
+            partyName: partyName,
+            user: user,
+          });
+        }}
+      >
         <Text style={styles.btnText}>Create Room</Text>
       </TouchableOpacity>
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20
+    padding: 20,
   },
-    input: {
-        height: 60,
-        padding: 8,
-        fontSize: 16
-    },
-    btn: {
-        backgroundColor: '#c2bad8',
-        padding: 9,
-        marginBottom: 200
-    },
-    btnText: {
-        color: 'darkslateblue',
-        fontSize: 20,
-        textAlign: 'center'
-    },
-    dollar: {
-      height: 50,
-      width: 50,
-      justifyContent: 'center',
-      borderRadius: 10
-    },
+  input: {
+    height: 60,
+    padding: 8,
+    fontSize: 16,
+  },
+  btn: {
+    backgroundColor: "#c2bad8",
+    padding: 9,
+    marginBottom: 200,
+  },
+  btnText: {
+    color: "darkslateblue",
+    fontSize: 20,
+    textAlign: "center",
+  },
+  dollar: {
+    height: 50,
+    width: 50,
+    justifyContent: "center",
+    borderRadius: 10,
+  },
 });
 
 export default Search;
