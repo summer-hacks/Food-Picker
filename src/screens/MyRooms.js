4c1cf5cefb11676043b7a5db650477ccf00f7ddb
Ashley Ahn <ashleyjsahn@gmail.com>
2020-08-03 17:24:39 -0700

    getting notif tokens

--- src/screens/MyRooms.js
+++ src/screens/MyRooms.js
@@ -11,15 +11,54 @@ import { COLOR_PRIMARY_LIGHT, COLOR_SECONDARY, FONT_NORMAL } from "../common";
 import Container from "../components/Container";
 import BigHeader from "../components/BigHeader";
 import StepHeader from "../components/StepHeader";
-
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 import { heightPercentageToDP } from "react-native-responsive-screen";
+
 function MyRooms({ route, navigation }) {
   const [user, setUser] = useState({});
   const [rooms, setRooms] = useState([]);
 
+  // notifications
+  // let currentUser = firebase.auth().currentUser;
+
   useEffect(() => {
     const currentUser = firebase.auth().currentUser;
-
+    async function registerForPushNotificationsAsync() {
+      const { status: existingStatus } = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+      );
+      let finalStatus = existingStatus;
+      
+      // only ask if permissions have not already been determined, because
+      // iOS won't necessarily prompt the user a second time.
+      if (existingStatus !== 'granted') {
+        // Android remote notification permissions are granted during the app
+        // install, so this will only ask on iOS
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        console.log('status', status);
+        finalStatus = status;
+      }
+      // Stop here if the user did not grant permissions
+      if (finalStatus !== 'granted') {
+        return;
+      }
+  
+      try {
+        // Get the token that uniquely identifies this device
+        let token = await Notifications.getExpoPushTokenAsync();
+        console.log('token', token);
+  
+        // POST the token to your backend server from where you can retrieve it to send push notifications.
+        firebase
+          .database()
+          .ref('users/' + currentUser.uid + '/push_token')
+          .set(token);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    registerForPushNotificationsAsync();
     if (currentUser) {
       setUser(currentUser);
       const userRef = firebase.database().ref("users/" + currentUser.uid);
@@ -48,6 +87,7 @@ function MyRooms({ route, navigation }) {
       });
     }
   }, []);
+
   return (
     <Container>
       <BigHeader title="My Rooms" />
