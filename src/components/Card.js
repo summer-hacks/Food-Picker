58b22990772f9f52059c80a0811a8589f7a49568
brians123 <brianssui@yahoo.com>
2020-08-04 23:53:04 -0400

    pop up for card tap and styling in progress

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,21 +12,23 @@ import {
 import { COLOR_PRIMARY } from "../common";
 
 const Card = ({ roomId, restaurant, navigation }) => {
-  const [visibility, setVisibility] = useState(false);
   return (
-    <View style={styles.bigContainer}>
+    <View style={styles.container}>
 
-      <View style={styles.container}>
-
-        <Image
-          style={{ width: 250, height: 250 }}
-          source={{
-            uri: restaurant.image_url,
-          }}
-        />
+      <Image
+        style={{ height: 250, width: 250, borderWidth: 2, borderRadius: 5 }}
+        source={{
+          uri: restaurant.image_url,
+        }}
+      />
+      <View style={{ alignSelf: 'flex-start', marginLeft: 37, marginTop: -15 }}>
         <Text style={styles.text}>{restaurant.name} </Text>
+        <Text style={{ fontSize: hp('1.75%'), color: 'grey' }}>{restaurant.categories[0].title}</Text>
+        {/* <Text>{restaurant.location.display_address[0]} </Text>
+          <Text >{restaurant.location.display_address[restaurant.location.display_address.length - 1]} </Text> */}
       </View>
     </View>
+
   );
 };
 
@@ -39,29 +41,42 @@ const styles = StyleSheet.create({
   },
   text: {
     fontFamily: "karla-bold",
-    fontSize: hp("2%"),
+    fontSize: hp("3.5%"),
     marginTop: hp("4%"),
   },
   container: {
+    // justifyContent: "center",
+    // alignItems: "center",
+    // alignSelf: "center",
+    // flex: 1,
+    // width: wp("80%"),
+    // height: hp("45%"),
+    // position: "absolute",
+    // backgroundColor: "white",
+    // borderRadius: 10,
+    // borderColor: "black",
+    // shadowColor: "gray",
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 6,
+    // },
+    // shadowOpacity: 0.05,
+    // shadowRadius: 8.3,
+    // elevation: 0,
+    flex: 1,
+    borderWidth: 3,
+    width: wp("80%"),
+    height: hp("45%"),
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: 'black',
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
     justifyContent: "center",
     alignItems: "center",
     alignSelf: "center",
-    flex: 1,
-    width: wp("80%"),
-    height: hp("45%"),
+    backgroundColor: 'white',
     position: "absolute",
-    backgroundColor: "white",
-    borderRadius: 10,
-    borderColor: "black",
-    borderWidth: 3,
-    shadowColor: "gray",
-    shadowOffset: {
-      width: 0,
-      height: 6,
-    },
-    shadowOpacity: 0.05,
-    shadowRadius: 8.3,
-    elevation: 0,
   },
   btnContainer: {
     marginTop: hp("50%"),
