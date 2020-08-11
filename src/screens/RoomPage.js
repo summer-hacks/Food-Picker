bfc5657b6a080eb66e59b85e96a71ff016ebffff
janethuangg <jh6248@nyu.edu>
2020-08-11 14:25:48 -0400

    added card detail popup to matches

--- src/screens/RoomPage.js
+++ src/screens/RoomPage.js
@@ -1,28 +1,37 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { StyleSheet, FlatList, Text, View } from 'react-native';
-import firebase from '../../firebase.js';
-import Container from '../components/Container';
-import BigHeader from '../components/BigHeader';
-import StepHeader from '../components/StepHeader';
-import { FONT_NORMAL, COLOR_PRIMARY } from '../common.js';
+import React, { useState, useRef, useEffect } from "react";
+import { StyleSheet, FlatList, Text, View } from "react-native";
+import firebase from "../../firebase.js";
+import Modal from "react-native-modal";
+import CardDetail from "../components/CardDetail";
+import Container from "../components/Container";
+import BigHeader from "../components/BigHeader";
+import StepHeader from "../components/StepHeader";
+import {
+  FONT_NORMAL,
+  COLOR_PRIMARY,
+  DEVICE_HEIGHT,
+  DEVICE_WIDTH,
+} from "../common.js";
 import {
   heightPercentageToDP,
   widthPercentageToDP,
-} from 'react-native-responsive-screen';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import LottieView from 'lottie-react-native';
-
+} from "react-native-responsive-screen";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import LottieView from "lottie-react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 function RoomPage({ route, navigation }) {
   const { room } = route.params;
   const [matches, setMatches] = useState([]);
+  const [index, setIndex] = useState(0);
   const [numCompleted, setCompleted] = useState(0);
   const [partySize, setSize] = useState(0);
+  const [visibility, setVisibility] = useState(false);
 
   // get matches if everyone has finished swiping
   useEffect(() => {
-    const roomRef = firebase.database().ref('rooms/' + room.roomId);
-    roomRef.once('value', (snap) => {
+    const roomRef = firebase.database().ref("rooms/" + room.roomId);
+    roomRef.once("value", (snap) => {
       const room = snap.val();
       setCompleted(room.numCompleted);
       setSize(room.partySize);
@@ -32,17 +41,16 @@ function RoomPage({ route, navigation }) {
           restaurants.push(room.restaurants[key]);
         });
         const matches = restaurants.filter((res) => res.yes === room.partySize);
-        setMatches(matches);
+        setMatches(matches.map((match, index) => ({ ...match, index })));
       }
     });
   }, []);
   // displays matches in a list if all users have finished swiping (a completed room)
-
   return (
     <Container>
-      <BigHeader title={room.name + '\n(' + room.roomId + ')'} />
+      <BigHeader title={room.name + "\n(" + room.roomId + ")"} />
       {numCompleted === partySize ? (
-        <StepHeader step='Here are your matches!' />
+        <StepHeader step="Here are your matches!" />
       ) : (
         <View style={styles.animationContainer}>
           <StepHeader
@@ -53,31 +61,53 @@ function RoomPage({ route, navigation }) {
             style={{
               width: 400,
               height: 400,
-              backgroundColor: 'white',
+              backgroundColor: "white",
             }}
-            source={require('../../assets/7093-roll-it-chicken-roll.json')}
+            source={require("../../assets/7093-roll-it-chicken-roll.json")}
           />
         </View>
       )}
       {numCompleted === partySize ? (
-        <FlatList
-          data={matches}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.inline}>
-              <Icon
-                color={COLOR_PRIMARY}
-                name='heart'
-                size={heightPercentageToDP(1.5)}
-                style={{
-                  marginRight: widthPercentageToDP(3),
-                  marginBottom: heightPercentageToDP(4),
-                }}
+        <View>
+          <Modal
+            isVisible={visibility}
+            backdropOpacity={1}
+            backdropColor="white"
+            height={DEVICE_HEIGHT}
+            width={DEVICE_WIDTH}
+          >
+            <View style={{ flex: 1, margin: -20.75 }}>
+              <CardDetail
+                restaurant={matches[index]}
+                closeCard={setVisibility}
               />
-              <Text style={styles.txt}>{item.name}</Text>
             </View>
-          )}
-        />
+          </Modal>
+          <FlatList
+            data={matches}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={styles.inline}
+                onPress={() => {
+                  setIndex(item.index);
+                  setVisibility(true);
+                }}
+              >
+                <Icon
+                  color={COLOR_PRIMARY}
+                  name="heart"
+                  size={heightPercentageToDP(1.5)}
+                  style={{
+                    marginRight: widthPercentageToDP(3),
+                    marginBottom: heightPercentageToDP(4),
+                  }}
+                />
+                <Text style={styles.txt}>{item.name}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
       ) : (
         <Text style={styles.txt}></Text>
       )}
@@ -92,17 +122,17 @@ const styles = StyleSheet.create({
     marginBottom: heightPercentageToDP(4),
   },
   animationContainer: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     flex: 1,
   },
   inline: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignSelf: 'flex-start',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "flex-start",
+    justifyContent: "space-between",
   },
 });
 export default RoomPage;
